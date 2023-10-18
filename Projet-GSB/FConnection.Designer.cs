633ae86cbe64b5c4e7229b258daabe0ac777566a
Adnan <adnan.mahboubi03@gmail.com>
2023-10-18 08:48:01 +0200

    création des champs 'identifiant' et 'mot de passe'  et d'un bouton 'connexion''

--- Projet-GSB/FConnection.Designer.cs
+++ Projet-GSB/FConnection.Designer.cs
@@ -28,19 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Identifiant = new System.Windows.Forms.Label();
+            this.mdp = new System.Windows.Forms.Label();
+            this.connexion = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 89);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(334, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 149);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(334, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // Identifiant
+            // 
+            this.Identifiant.AutoSize = true;
+            this.Identifiant.Location = new System.Drawing.Point(31, 92);
+            this.Identifiant.Name = "Identifiant";
+            this.Identifiant.Size = new System.Drawing.Size(53, 13);
+            this.Identifiant.TabIndex = 2;
+            this.Identifiant.Text = "Identifiant";
+            this.Identifiant.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // mdp
+            // 
+            this.mdp.AutoSize = true;
+            this.mdp.Location = new System.Drawing.Point(23, 152);
+            this.mdp.Name = "mdp";
+            this.mdp.Size = new System.Drawing.Size(71, 13);
+            this.mdp.TabIndex = 3;
+            this.mdp.Text = "Mot de passe";
+            // 
+            // connexion
+            // 
+            this.connexion.Location = new System.Drawing.Point(100, 261);
+            this.connexion.Name = "connexion";
+            this.connexion.Size = new System.Drawing.Size(70, 25);
+            this.connexion.TabIndex = 4;
+            this.connexion.Text = "Connexion";
+            this.connexion.UseVisualStyleBackColor = true;
             // 
             // FConnection
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.connexion);
+            this.Controls.Add(this.mdp);
+            this.Controls.Add(this.Identifiant);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Name = "FConnection";
             this.Text = "FConnexion";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label Identifiant;
+        private System.Windows.Forms.Label mdp;
+        private System.Windows.Forms.Button connexion;
     }
 }
--- Projet-GSB/FConnection.cs
+++ Projet-GSB/FConnection.cs
@@ -16,5 +16,10 @@ namespace Projet_GSB
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }

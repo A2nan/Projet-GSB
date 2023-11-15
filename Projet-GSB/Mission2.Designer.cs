adc26bd9bb43a3e71b9f4f3fd6a8ca283616fcec
Maxence Denizane <maxence.deniz@gmail.com>
2023-11-15 10:51:39 +0100

    modif text design mission 2

--- Projet-GSB/Mission2.Designer.cs
+++ Projet-GSB/Mission2.Designer.cs
@@ -55,16 +55,16 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Ajout rapport";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(151, 415);
+            this.button2.Location = new System.Drawing.Point(135, 415);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(93, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Modif rapport";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -73,7 +73,7 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Quitter";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
@@ -146,57 +146,57 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 41);
+            this.label2.Location = new System.Drawing.Point(18, 41);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 13;
-            this.label2.Text = "label2";
+            this.label2.Text = "PRENOM";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(225, 15);
+            this.label3.Location = new System.Drawing.Point(202, 15);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.Size = new System.Drawing.Size(58, 13);
             this.label3.TabIndex = 14;
-            this.label3.Text = "label3";
+            this.label3.Text = "ADRESSE";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(225, 41);
+            this.label4.Location = new System.Drawing.Point(191, 41);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.Size = new System.Drawing.Size(69, 13);
             this.label4.TabIndex = 15;
-            this.label4.Text = "label4";
+            this.label4.Text = "MATRICULE";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(433, 16);
+            this.label5.Location = new System.Drawing.Point(377, 15);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.Size = new System.Drawing.Size(91, 13);
             this.label5.TabIndex = 16;
-            this.label5.Text = "label5";
+            this.label5.Text = "Date d\'embauche";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(511, 165);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.Size = new System.Drawing.Size(36, 13);
             this.label6.TabIndex = 17;
-            this.label6.Text = "label6";
+            this.label6.Text = "DATE";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(36, 15);
+            this.label1.Location = new System.Drawing.Point(39, 15);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(32, 13);
             this.label1.TabIndex = 18;
             this.label1.Tag = "";
-            this.label1.Text = "label1";
+            this.label1.Text = "NOM";
             // 
             // Mission2
             // 

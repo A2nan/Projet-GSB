44edfc8ba773a72fa96df620d211bc31da61fcbf
BradleyMiguel <bradley.miguel02@gmail.com>
2023-11-15 10:00:51 +0100

    frais

--- Projet-GSB/Mission 3.cs
+++ Projet-GSB/Mission 3.cs
@@ -16,5 +16,11 @@ namespace Projet_GSB
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FrainsEngages fEngages = new FrainsEngages();
+            fEngages.Show();
+        }
     }
 }
